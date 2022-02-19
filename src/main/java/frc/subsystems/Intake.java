3cd3e6c67985680d9530ee5eb6a349cfc589f682
Linski <molinski@gideonsp.com>
2022-02-19 11:48:09 -0800

    added intake functionality, streamlined intake

--- src/main/java/frc/subsystems/Intake.java
+++ src/main/java/frc/subsystems/Intake.java
@@ -3,15 +3,17 @@ package frc.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Controller;
 import frc.robot.RobotMap;
 public class Intake {
     // motors
     private final CANSparkMax lift, intake;
-
+    private int intakeMode = 1; // either -1 or 1 
     // intake state
     private LiftPosition position;
     private boolean isOn = false;
-
+    private XboxController controller = Controller.getInstance().getController();
     // singleton instance
     private static final Intake instance = new Intake();
 
@@ -21,7 +23,27 @@ public class Intake {
         lift = new CANSparkMax(RobotMap.INTAKE_MOTOR_LIFT, MotorType.kBrushless);
         intake = new CANSparkMax(RobotMap.INTAKE_MOTOR_INTAKE, MotorType.kBrushless);
     }
-    
+    public void spin(){
+        while (controller.getLeftTriggerAxis()>0) {
+            intake.set(controller.getLeftTriggerAxis());
+            try {
+                Thread.sleep(10);
+            } catch (Exception err) {
+                err.printStackTrace();
+            }
+        }
+        intake.stopMotor();
+    }
+    public void toggleIntakeDirection(){
+        if(intakeMode == 1){
+            intakeMode = 0;
+        }else if(intakeMode == 0){
+            intakeMode = 1;
+        }
+    }
+    public int getIntakeDirection(){
+        return intakeMode;
+    }
     public void setIntakeState(boolean on) { //set Intake to state x
         isOn = on;
         intake.set(isOn ? 1 : 0);
